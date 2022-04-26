06c298e25e8a046b9a8cd410051b2473ecc9513f
Joel Trinidad <jth_2929@hotmail.com>
2022-04-25 21:39:31 -0500

    new rute that return an object

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Usando objeto express
 const express = require('express');
+const req = require('express/lib/request');
 // App de Express
 const app = express();
 // Puerto en que vamos a ver nuestra app: localhost:3000
@@ -10,9 +11,17 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 })
 
+// Respondiendo texto
 // agregando nueva ruta localhost:3000/launchx
 app.get('/launchx', (req, res) => {
     res.send('Bienvenidos a launchx');
+})
+
+// Regresando un objeto
+// localhost:3000/explorersInNode
+app.get('/explorersInNode', (req, res) => {
+    const explorer = {name: "Explorer", msg: "Hello"}
+    res.send(explorer)
 })
 
 // Con esto inicializamos esta app
